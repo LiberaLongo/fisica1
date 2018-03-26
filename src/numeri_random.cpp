807c4702b6525c8ec1647490c9ca62611ec38a42
LiberaLongo <liberalongo@gmail.com>
2018-03-26 16:56:15 +0200

    modify max val

--- src/numeri_random.cpp
+++ src/numeri_random.cpp
@@ -13,7 +13,7 @@ constexpr size_t nrandom = 100;
 constexpr int min_int = -2;
 constexpr int max_int = 4;
 constexpr double min_double = -1.5;
-constexpr double max_double = 3.3;
+constexpr double max_double = 3.0;
 
 int main ()
 {
@@ -21,7 +21,7 @@ int main ()
 
   int irandom;
   double drandom;
-  
+
   std::ofstream file_int;
   file_int.open ("integers.txt");
   std::ofstream file_real;
@@ -32,7 +32,7 @@ int main ()
 
   for (size_t i=0; i < nrandom; ++i)
   {
-    //irandom = i1 + int(1.0*(i2-i1+1)*rand()/(RAND_MAX+1.0)); 
+    //irandom = i1 + int(1.0*(i2-i1+1)*rand()/(RAND_MAX+1.0));
     //irandom = rand()%(i2-i1+1)+i1;
     drandom = rand()/double(RAND_MAX);  /* random number between 0.0 and 1.0 */
     drandom *= (max_double - min_double);
@@ -42,7 +42,7 @@ int main ()
     file_int  << setw(3) << irandom << std::endl; /* output to a file */
   }
 
-  cout << endl << "Numeri semi-casuali reali tra " << min_double << " e " << max_double << endl; 
+  cout << endl << "Numeri semi-casuali reali tra " << min_double << " e " << max_double << endl;
   for (size_t i=0; i < nrandom; ++i)
   {
     drandom = rand()/double(RAND_MAX);  /* random number between 0.0 and 1.0 */
@@ -54,5 +54,3 @@ int main ()
 
   return 0;
 }
-
-
